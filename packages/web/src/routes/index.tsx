c84b934630619bc5c55b5f38dda8a1259c0b4380
Dinh Vo Bao Chau <chaudvb95uc@gmail.com>
2020-12-02 04:30:03 +0700

    yarn install @apollo/client, react-router-dom and types packages

---
+++ packages/web/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from "../App";
+
+import { Register } from "../components/Register";
+
+export const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact={true} path="/" component={App} />
+      <Route exact={true} path="/register" component={Register} />
+    </Switch>
+  </BrowserRouter>
+);
